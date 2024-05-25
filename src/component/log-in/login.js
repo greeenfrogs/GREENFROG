666c0876812d557905b7d6c054aabb5b3d4f97f2
delightedsky <bhm0208@hufs.ac.kr>
2024-05-25 19:33:11 +0900

    login issue

---
+++ src/component/log-in/login.js
@@ -0,0 +1,10 @@
+// login.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Login() {
+    <div>
+        <h2>LOGIN PAGE</h2>
+    </div>
+}
+ReactDOM.render(<Login />, document.getElementById('login-root'));
