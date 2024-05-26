0800cc15d1a8735d0e1b8207604cb6c125470166
Hongjunior <seokjun09@gmail.com>
2024-05-26 22:26:16 +0900

    글쓰기창 구현

--- src/component/Request.js
+++ src/component/Request.js
@@ -9,20 +9,20 @@ import write_button_svg from './buttonfile/write_button.svg';
 import frog_empty_svg from './buttonfile/frog_empty.svg';
 import frog_click_svg from './buttonfile/frog_click.svg';
 import correct_button_svg from './buttonfile/correct_button.svg';
-import Searchbar from './Request_func/Searchbar';
-import Searchtype from './Request_func/Searchtype';
+import Pagination from './Pagination';
 
 export default function Request() {
-    const [menu, setMenu] = useState('');
+    const navigate = useNavigate();
     const [frogClicked, setFrogClicked] = useState(Array.from({ length: 11 }, () => false));
     const [currentPage, setCurrentPage] = useState(1);
+    const [selectedMenu, setSelectedMenu] = useState('전체'); // '전체'를 초기값으로 선택
     const totalPages = 10;
-    const navigate = useNavigate(); // useNavigate 훅을 가져옵니다
 
-    const handleMenuClick = (selectedMenu) => {
-        setMenu(selectedMenu);
-        if (selectedMenu === 'write') {
+    const handleMenuClick = (option) => {
+        if (option === 'write') {
             navigate('/request/writemode'); // 페이지 이동
+        } else {
+            setSelectedMenu(option); // 클릭된 옵션을 상태로 설정
         }
     };
 
@@ -32,95 +32,78 @@ export default function Request() {
         setFrogClicked(updatedFrogClicked);
     };
 
-    const handleNextPage = () => {
-        setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
-    };
-
-    const handlePrevPage = () => {
-        setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
-    };
-
-    const handleFirstPage = () => {
-        setCurrentPage(1);
-    };
-
-    const handleLastPage = () => {
-        setCurrentPage(totalPages);
-    };
-
     const handlePageClick = (page) => {
         setCurrentPage(page);
     };
 
-    const renderPageNumbers = () => {
-        const pages = [];
-        const visiblePages = 10;
-
-        for (let page = 1; page <= totalPages; page++) {
-            pages.push(
-                <button key={page} onClick={() => handlePageClick(page)} className={page === currentPage ? 'active' : ''}>
-                    {page}
-                </button>
-            );
-
-            if (pages.length >= visiblePages) break;
-        }
-
-        return pages;
-    };
-
     return (
         <div>
-            <div className='introduce_container'>
-                <img className='introduce_image' src={intro_req} alt='introduce_req' />
-                <div className="menu" style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <div style={{ display: 'flex' }}>
-                        <img className="Searchbar" src={searchbar_svg} alt="searchbar" style={{ marginTop: '12px' }} onClick={() => handleMenuClick('memberInfo')} />
-                        <div className="dropdown" style={{ position: 'relative', display: 'inline-block' }}>
-                            <img className="Searchtype" src={searchtype_svg} alt="searchtype" style={{ marginTop: '12px' }} onClick={() => handleMenuClick('searchtype')} />
-                            <div className="dropdown-content" style={{ position: 'absolute', top: '100%', zIndex: 1 }}>
-                                <a className="heading3" href="#">전체</a>
-                                <a className="heading3" href="#">즐겨찾기</a>
-                                <a className="heading3" href="#">진행중</a>
-                                <a className="heading3" href="#">프론트</a>
-                                <a className="heading3" href="#">백</a>
-                                <a className="heading3" href="#">UI/UX</a>
-                            </div>
+            <img className='introduce_image' src={intro_req} alt='introduce_req' />
+            <div className="header_req">
+                <div className="menu-left">
+                    <img 
+                        className="Searchbar" 
+                        src={searchbar_svg} 
+                        alt="searchbar" 
+                    />
+                    <div className="dropdown">
+                        <img 
+                            className="Searchtype" 
+                            src={searchtype_svg} 
+                            alt="searchtype"
+                        />
+                        <div className="dropdown-content">
+                            <a 
+                                className={`heading3 ${selectedMenu === '전체' ? 'selected' : ''}`} // 선택된 메뉴인 경우에만 'selected' 클래스 추가
+                                href="/all" 
+                                onClick={() => handleMenuClick('전체')} // 클릭 이벤트 핸들러 추가
+                            >
+                                전체
+                            </a>
+                            <a 
+                                className={`heading3 ${selectedMenu === '의뢰처' ? 'selected' : ''}`} 
+                                href="/favorites" 
+                                onClick={() => handleMenuClick('의뢰처')}
+                            >
+                                의뢰처
+                            </a>
+                            <a 
+                                className={`heading3 ${selectedMenu === '의뢰 제목' ? 'selected' : ''}`} 
+                                href="/inProgress" 
+                                onClick={() => handleMenuClick('의뢰 제목')}
+                            >
+                                의뢰 제목
+                            </a>
                         </div>
                     </div>
-                    <img className="WriteButton" src={write_button_svg} alt="write_button" style={{ marginBottom: '10px' }} onClick={() => handleMenuClick('write')} />
                 </div>
+                <img 
+                    className="WriteButton" 
+                    src={write_button_svg} 
+                    alt="write_button"
+                    onClick={() => handleMenuClick('write')}
+                />
             </div>
-            <div className="wrapper">
-                <div className="menu" style={{ display: 'flex', flexDirection: 'row' }}>
-                    {menu === 'memberInfo' && <Searchbar />}
-                    {menu === 'searchtype' && <Searchtype />}
-                </div>
-            </div>
-            <div style={{ margin: '0 30px' }}>
-                <table style={{ width: '100%', borderCollapse: 'collapse', marginTop: '-120px' }}>
+            <div>
+                <table className="request-table">
                     <tbody>
                         {Array.from(Array(11).keys()).map(row => (
-                            <tr key={row} style={{ borderBottom: '1px solid var(--theme---primary---light)' }}>
+                            <tr key={row} className="table-row">
                                 {Array.from(Array(7).keys()).map(col => (
-                                    <td
-                                        key={col}
-                                        className={(col === 0 && row !== 0 && row !== 10) ? 'heading3' : ''}
-                                        style={{ border: 'none', padding: '10px' }}
-                                    >
+                                    <td key={col} className={(col === 0 && row !== 0 && row !== 10) ? 'heading3' : ''}>
                                         {(col === 0 && row === 0) ? '' :
                                             (col === 6 && row === 0) ? '' :
                                             (col === 0) ?
                                                 <img src={frogClicked[row] ? frog_click_svg : frog_empty_svg}
                                                     alt={`frog_${row}-${col}`}
-                                                    style={{ marginTop: '10px' }}
+                                                    className="frog-icon"
                                                     onClick={() => handleFrogClick(row)} /> :
-                                                (col === 6) ? <img src={correct_button_svg} alt={`correct_button_${row}-${col}`} style={{ marginTop: '10px' }} /> :
+                                                (col === 6) ? <img src={correct_button_svg} alt={`correct_button_${row}-${col}`} className="correct-icon" /> :
                                                 (col === 1 && row === 0) ? <span className="heading3">가격</span> :
-                                                    (col === 2 && row === 0) ? <span className="heading3">인원</span> :
+                                                    (col === 2 && row === 0) ? <span className="heading3">유형</span> :
                                                         (col === 3 && row === 0) ? <span className="heading3">의뢰처</span> :
-                                                            (col === 4 && row === 0) ? <span className="heading3">의뢰제목</span> :
-                                                                (col === 5 && row === 0) ? <span className="heading3">진행 중</span> :
+                                                            (col === 4 && row === 0) ? <span className="heading3">의뢰 제목</span> :
+                                                                (col === 5 && row === 0) ? <span className="heading3">진행 상태</span> :
                                                                     `${row}-${col}`}
                                     </td>
                                 ))}
@@ -129,13 +112,11 @@ export default function Request() {
                     </tbody>
                 </table>
             </div>
-            <div className="pagination" style={{ margin: '50px 0' }}>
-                <button onClick={handleFirstPage}>«</button>
-                <button onClick={handlePrevPage}>‹</button>
-                {renderPageNumbers()}
-                <button onClick={handleNextPage}>›</button>
-                <button onClick={handleLastPage}>»</button>
-            </div>
+            <Pagination 
+                currentPage={currentPage}
+                totalPages={totalPages}
+                handlePageClick={handlePageClick}
+            />
         </div>
     );
 }
