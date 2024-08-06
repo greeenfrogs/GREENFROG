e36b24faefa681d117ce8fc7461b3d66b849ae10
K0seoyoung <koseo12@hufs.ac.kr>
2024-08-07 00:10:43 +0900

    할 수 있다

---
+++ src/component/Free_writemode.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Free_writemode.css';
+import intro_req from './figfile/intro_req.svg';
+import front from './buttonfile/front.svg';
+import back from './buttonfile/back.svg';
+import uiux from './buttonfile/uiux.svg';
+import WriteButton from './buttonfile/write_button.svg';
+
+export default function FreeWritemode() {
+  const handleSubmit = () => {
+    console.log('Submit button clicked');
+    // 의뢰 제출 로직을 여기에 추가하세요.
+  };
+
+  return (
+    <div className='introduce_container'>
+      <img className='introduce_image' src={intro_req} alt='introduce_req' />
+      <div className="title-container">
+        <h1 className="heading2">제목</h1>
+        <div className="name-box"></div>
+      </div>
+      <div className="type-container">
+        <h2 className="heading2">유형</h2>
+        <div className="icon-container">
+          <img className='type-icon' src={front} alt='front_icon' />
+          <img className='type-icon' src={back} alt='back_icon' />
+          <img className='type-icon' src={uiux} alt='uiux_icon' />
+        </div>
+      </div>
+      <div className="price-container">
+        <h2 className="heading2">가격 제시</h2>
+        <button className="price-button">원</button>
+        <span className="price-text">제시</span>
+      </div>
+      <div className="write-container">
+        <input type='text' placeholder='예시글자임: 수박게임 만들고 싶다 어쩌구저쩌구' />
+        
+      </div>
+      <div className="client-container">
+        <h2 className="heading2">의뢰처</h2>
+        <div className="email-box"></div>
+      </div>
+      <div className="submit-container">
+        <img
+          className='WriteButton'
+          src={WriteButton}
+          alt='WriteButton'
+          onClick={handleSubmit}
+        />
+      </div>
+    </div>
+  );
+}
